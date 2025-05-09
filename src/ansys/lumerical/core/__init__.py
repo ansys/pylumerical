5da9b5b9c4b0191b542f8c397a7bc36f628954b8
Dylan McGuire <dylan.mcguire@ansys.com>
2025-05-09 10:23:14 -0700

    Initial clean up and init for lumapi in ansys.api.lumerical; renamed module to ansys.lumerical.core

--- src/ansys/lumerical/core/__init__.py
+++ src/ansys/lumerical/core/__init__.py
@@ -1,12 +1,16 @@
 """
 lumerical.
 
-Library
+Core
 """
 
 try:
     import importlib.metadata as importlib_metadata
 except ModuleNotFoundError:
     import importlib_metadata
+
+from ansys.api.lumerical.lumapi import FDTD, MODE, INTERCONNECT, DEVICE, InteropPaths
+
+InteropPaths.autoInitInstallPath()  # warns if the path cannot be guessed
 
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))

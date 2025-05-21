0cfbce7de0de346bbe44b4017d1b7a3b4e9e4cd6
Dylan McGuire <dylan.mcguire@ansys.com>
2025-05-21 15:34:43 -0700

    License headers from pre-commit hooks

--- tests/test_MODE_start_session.py
+++ tests/test_MODE_start_session.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2021 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 """A test for starting a MODE session and verifying the connection using the Lumerical API."""
 
 import ansys.lumerical.core as lumapi  # Import the lumapi module
@@ -17,14 +39,11 @@ def test_MODE_start_session():
     # Start a MODE session
     with lumapi.MODE(hide=True) as mode:
         # Add a rectangle
-        rect_properties = {
-            "name": "test_rect",
-            "x span": 3e-6
-        }
+        rect_properties = {"name": "test_rect", "x span": 3e-6}
         mode.addrect(rect_properties)
-        
+
         # Query the value of the property
         x_span_value = mode.getnamed("test_rect", "x span")
-        
+
         # Check if the set value and read value are the same
         assert x_span_value == 3e-6

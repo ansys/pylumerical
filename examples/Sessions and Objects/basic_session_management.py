8769c0cab71c528b57a0edaf6a1b967b0a53e512
annawirthsingh-ansys <anna.wirth-singh@ansys.com>
2025-07-07 10:42:34 -0700

    Initial commit of simple examples
    1. Basic session management (3 ways)
    2. First FDTD simulation with LSF style
    3. First FDTD simulation with Pythonic style

---
+++ examples/Sessions and Objects/basic_session_management.py
@@ -0,0 +1,68 @@
+"""
+
+Basic Session Management
+------------------------------------------------------
+This example demonstrates how to initialize a local Lumerical session.
+PyLumerical interacts with Lumerical products through sessions. 
+
+
+Prerequesites: Valid FDTD license is required. 
+"""
+
+###############################################################################
+#
+# Perform required imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+import ansys.lumerical.core as lumapi
+import time
+
+###############################################################################
+#
+# Open an interactive session
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+fdtd = lumapi.FDTD()
+# Wait for 5 seconds, then add fdtd region
+time.sleep(5)
+fdtd.addfdtd()
+time.sleep(2)
+fdtd.close()
+
+mode = lumapi.MODE()
+# Wait for 5 seconds, then close
+time.sleep(5)
+mode.close()
+
+# Load a session but hide the application window
+fdtd = lumapi.FDTD(hide=True)
+fdtd.close()
+
+###############################################################################
+#
+# Use the "with" context manager
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+with lumapi.FDTD() as fdtd:
+    fdtd.addfdtd()
+    time.sleep(2)
+# FDTD closes automatically
+
+###############################################################################
+#
+# Session wrapped in a function
+# Simple example - returns the number of grid cells in FDTD region for set span
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+def get_x_cells(fdtd_span):
+    fdtd = lumapi.FDTD()
+    # adds fdtd region with span set by fdtd_span
+    fdtd.addfdtd(dimension = "3D", x_span = fdtd_span, y_span = fdtd_span, z_span = fdtd_span)
+    x = fdtd.getresult("FDTD","x") # Get x coordinates of created FDTD region
+    x_cells = len(x)
+    return x_cells
+
+# Test the function and print out the result
+test = get_x_cells(1e-6)
+print(test)
+
---
+++ examples/Sessions and Objects/fdtd_example1_lsf.py
@@ -0,0 +1,64 @@
+"""
+
+A simple example to demonstrate using PyLumerical
+------------------------------------------------------
+Sets up and runs a basic FDTD simulation. E field results are plotted in Lumerical.
+
+
+Prerequesites: Valid FDTD license is required. 
+"""
+
+###############################################################################
+#
+# Perform required imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+import ansys.lumerical.core as lumapi
+
+# Set hide = True to hide the Lumerical GUI. 
+fdtd = lumapi.FDTD(hide=False)
+ 
+# Set up simulation region
+fdtd.addfdtd()
+fdtd.set("x",0)
+fdtd.set("x span",8e-6)
+fdtd.set("y",0)
+fdtd.set("y span",8e-6)
+fdtd.set("z",0.25e-6)
+fdtd.set("z span",0.5e-6)
+ 
+# Set up source
+fdtd.addgaussian()
+fdtd.set("injection axis","z")
+fdtd.set("direction","forward")
+fdtd.set("x",0)
+fdtd.set("x span",16e-6)
+fdtd.set("y",0)
+fdtd.set("y span",16e-6)
+fdtd.set("z",0.2e-6)
+fdtd.set("use scalar approximation",1)
+fdtd.set("waist radius w0",2e-6)
+fdtd.set("distance from waist",0)
+fdtd.setglobalsource("wavelength start",1e-6)
+fdtd.setglobalsource("wavelength stop",1e-6)
+ 
+# Set up monitor
+fdtd.adddftmonitor()
+fdtd.set("monitor type","2D Z-normal")
+fdtd.set("x",0)
+fdtd.set("x span",16e-6)
+fdtd.set("y",0)
+fdtd.set("y span",16e-6)
+fdtd.set("z",0.3e-6)
+ 
+# Run and save simulation
+fdtd.save("fdtd_tutorial.fsp")
+fdtd.run()
+
+# Retrieve and plot results
+E = fdtd.getresult("monitor","E")
+fdtd.visualize(E)
+
+# Keep session open until user clicks space bar
+fdtd.print("Example complete. Hit space bar to close.")
+fdtd.pause(60)

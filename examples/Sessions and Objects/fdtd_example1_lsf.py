97ac5b02d410dfdef8b9e16b725e034940b77b36
annawirthsingh-ansys <anna.wirth-singh@ansys.com>
2025-07-08 15:24:05 -0700

    Update examples/Sessions and Objects/fdtd_example1_lsf.py
    Co-authored-by: Kerry McAdams <58492561+klmcadams@users.noreply.github.com>

--- examples/Sessions and Objects/fdtd_example1_lsf.py
+++ examples/Sessions and Objects/fdtd_example1_lsf.py
@@ -1,7 +1,7 @@
 """
 
 A simple example to demonstrate using PyLumerical
-------------------------------------------------------
+-------------------------------------------------
 Sets up and runs a basic FDTD simulation. E field results are plotted in Lumerical.
 
 

81ead39444c5e552c7fc38f107827aee8536dee9
Luke Queenan <luke.queenan@ansys.com>
2025-12-22 10:53:56 -0800

    Bump version 0.2.0

--- src/ansys/lumerical/core/__init__.py
+++ src/ansys/lumerical/core/__init__.py
@@ -29,7 +29,7 @@ from ansys.api.lumerical.lumapi import DEVICE, FDTD, INTERCONNECT, MODE, Interop
 
 from . import autodiscovery
 
-__version__ = "0.1.dev0"
+__version__ = "0.2.0"
 """Lumerical API version."""
 
 if len(ansys.api.lumerical.lumapi.InteropPaths.LUMERICALINSTALLDIR) == 0:

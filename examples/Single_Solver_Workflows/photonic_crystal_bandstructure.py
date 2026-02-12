f8cf8301de398d9e6c0924b18ecf98138d70a450
annawirthsingh-ansys <anna.wirth-singh@ansys.com>
2026-02-11 20:59:09 -0800

    Update photonic_crystal_bandstructure.py
    Major updates to the photonic crystal bandstructure example to make it more Pythonic in nature and use the "with" context manager.

--- examples/Single_Solver_Workflows/photonic_crystal_bandstructure.py
+++ examples/Single_Solver_Workflows/photonic_crystal_bandstructure.py
@@ -1,21 +1,39 @@
-# # Photonic Crystal Bandstructure (FDTD)
-#
-# This example demonstrates a photonic crystal simulation utilizing a built-in Structure Group object.
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.18.1
+#   kernelspec:
+#     display_name: .venv
+#     language: python
+#     name: .venv
+# ---
+
+# This example demonstrates a photonic crystal simulation utilizing Structure and Analysis Group objects.
 # Based on: https://optics.ansys.com/hc/en-us/articles/360041566614-Rectangular-Photonic-Crystal-Bandstructure
 #
 #
-# In Part 1, we build the structure and set the FDTD simulation region. In this case, the spheres are holes (filled with air, n = 1)
-# and the background material is a simple dielectric material. Some advanced simulation objects, including the photonic crystal array
-# and bandstructure analysis groups, are imported from the Object Library. We run a single simulation and visualize the resulting spectrum.
+# In Part 1, we build the structure and set the FDTD simulation region.
+# In this case, the spheres are holes (filled with air, n = 1) and the background material is a simple dielectric material.
+# Some advanced simulation objects, including the dipole cloud source and bandstructure analysis groups, are imported from the Object Library.
+# We run a single simulation and visualize the resulting spectrum.
 #
-# In Part 2, we set up a series of sweeps to collect the resonant frequencies. In this example, we use the built-in sweep tool in Lumerical,
-# but the parameter sweeps could also be set up from Python. We then run the sweeps and plot the results.
+# In Part 2, we set up a series of sweeps to collect the resonant frequencies.
+# In this example, we use the built-in sweep tool in Lumerical, but the parameter sweeps could also be set up from Python.
+# We then run the sweeps and plot the results.
 #
 # Prerequisites: Valid FDTD license is required.
 
 # Perform required imports
 
 # +
+from collections import OrderedDict
+import itertools
+
 import matplotlib.pyplot as plt  # Only required for plotting
 import numpy as np
 
@@ -27,6 +45,9 @@ import ansys.lumerical.core as lumapi
 
 # +
 # Define parameters
+
+# Set filename for saving and loading
+filename = "photonic_crystal_bandstructure.fsp"
 
 # set desired period of the array (3D)
 ax = 0.5e-6
@@ -46,98 +67,77 @@ background_index = 3.6055
 # Define frequencies / wavelengths of interest
 f1 = 1e12  # THz
 f2 = 220e12  # THz
+
+# +
+# Initialize session and build simulation objects. Set hide = True to hide the Lumerical GUI.
+# This block will build an array of spheres for the photonic crystal structure.
+# We add an FDTD region, sources, and monitors. The sources and monitors are imported from the Object Library.
+# Finally, the simulation file is saved to the file name set earlier.
+
+with lumapi.FDTD(hide=False) as fdtd:
+    # Add rectangular lattice array of spheres
+    # Note you can also use Lumerical's built-in "rect_pc_3D" object using fdtd.addobject("rect_pc_3D")
+    # In this example, we construct the array programmatically
+    fdtd.addstructuregroup({"name": "rect_pc_3D"})
+    for ix, iy, iz in itertools.product(*map(range, (nx, ny, nz))):
+        objname = f"c{ix}{iy}{iz}"
+        fdtd.addsphere(
+            {"name": objname, "x": (ix - 1) * ax, "y": (iy - 1) * ay, "z": (iz - 1) * az, "radius": sphere_radius, "material": sphere_material}
+        )
+        fdtd.addtogroup("rect_pc_3D")  # Adds the recently created sphere to the structure group
+
+    # Add FDTD region
+    dx = 0.025e-6  # Mesh dx, dy, dz
+    fdtd_geometry_props = {"x": 0, "x span": ax, "y": 0, "y span": ay, "z": 0, "z span": az}
+    fdtd_mesh_props = {"index": background_index, "mesh type": "uniform", "dx": dx, "dy": dx, "dz": dx}
+    fdtd_boundary_props = {"x min bc": "Bloch", "y min bc": "Bloch", "z min bc": "Bloch", "set based on source angle": False, "bloch units": "SI"}
+    # Combine properties settings into one dictionary
+    fdtd_props = OrderedDict({**fdtd_geometry_props, **fdtd_mesh_props, **fdtd_boundary_props})
+
+    fdtd.addfdtd(properties=fdtd_props)
+
+    # Set up sources (dipole cloud) and monitors (bandstructure)
+    # These are both Analysis Groups available from the object library
+    dipole_geometry_props = {"n dipoles": 3, "lattice type": 3, "z span": az, "ax": ax, "ay": ay, "az": az, "a": az, "x": 0, "y": 0, "z": 0}
+    dipole_frequency_props = {"f1": f1, "f2": f2, "kx": 0.5, "ky": 0, "kz": 0}
+    dipole_props = OrderedDict({**dipole_geometry_props, **dipole_frequency_props})
+
+    fdtd.addobject("dipole_cloud", properties=dipole_props)
+
+    bandstructure_geometry_props = {"n monitors": 10, "x": 0, "x span": ax, "y": 0, "y span": ay, "z": 0, "z span": az}
+    bandstructure_frequency_props = {"f1": f1, "f2": f2}
+    bandstructure_props = OrderedDict({**bandstructure_geometry_props, **bandstructure_frequency_props})
+    fdtd.addobject("bandstructure", properties=bandstructure_props)
+
+    # zoom CAD view around simulation region
+    fdtd.select("FDTD")
+    fdtd.setview("extent")
+
+    fdtd.save(filename)
+    print("File saved to folder as: " + filename)
+
+# +
+# Open the file and run a single simulation. Visualize the spectrum.
+
+with lumapi.FDTD(filename, hide=False) as fdtd:
+    print("Simulation running...")
+    fdtd.run()
+    print("Run completed. Analyzing and plotting results...")
+    fdtd.runanalysis()
+
+    # Plot results
+    single_spectrum = fdtd.getresult("bandstructure", "spectrum")
+    spectrum = single_spectrum["fd"]
+    frequencies = single_spectrum["f"]
+
+    plt.figure()
+    plt.plot(frequencies, spectrum)
+    plt.title("Single Simulation Spectrum")
+    plt.xlabel("Frequency")
+    plt.show(block=False)
+    plt.pause(2)
+
 # -
-
-# Initialize session. Set hide = True to hide the Lumerical GUI.
-fdtd = lumapi.FDTD(hide=False)
-
-# +
-# Add objects
-
-# Add and set up rectangular lattice array of spheres
-fdtd.addobject("rect_pc_3D")
-fdtd.set("x", 0)
-fdtd.set("y", 0)
-fdtd.set("z", 0)
-fdtd.set("index", sphere_index)
-fdtd.set("material", sphere_material)
-fdtd.set("nx", nx)
-fdtd.set("ny", ny)
-fdtd.set("nz", nz)
-fdtd.set("ax", ax)
-fdtd.set("ay", ay)
-fdtd.set("az", az)
-fdtd.set("radius", sphere_radius)
-
-# +
-# Add and set up FDTD simulation region
-
-dx = 0.025e-6  # Mesh dx, dy, dz
-
-fdtd.addfdtd()
-fdtd.set("x", 0)
-fdtd.set("x span", ax)
-fdtd.set("y", 0)
-fdtd.set("y span", ay)
-fdtd.set("z", 0)
-fdtd.set("z span", az)
-fdtd.set("index", background_index)
-fdtd.set("mesh type", "uniform")
-fdtd.set("dx", dx)
-fdtd.set("dy", dx)
-fdtd.set("dz", dx)
-fdtd.set("x min bc", "Bloch")
-fdtd.set("y min bc", "Bloch")
-fdtd.set("z min bc", "Bloch")
-# Settings for bloch conditions
-# We generally use bandstructure units, but for the simulation region it's
-# most convenient to use SI units because kx and ky can have different
-# normalizations if the x and y spans are different.
-# A normalization factor of 2*pi/a is applied to k later in the sweeps.
-fdtd.set("set based on source angle", False)
-fdtd.set("bloch units", "SI")
-
-
-# +
-# Set up sources (dipole cloud) and monitors (bandstructure)
-# These are both Analysis Groups available from the object library
-
-fdtd.addobject("dipole_cloud")
-fdtd.set("f1", f1)
-fdtd.set("f2", f2)
-fdtd.set("n dipoles", 3)
-fdtd.set("kx", 0.5)
-fdtd.set("ky", 0)
-fdtd.set("kz", 0)
-fdtd.set("lattice type", 3)
-fdtd.set("z span", az)
-fdtd.set("ax", ax)
-fdtd.set("ay", ay)
-fdtd.set("az", az)
-fdtd.set("a", az)
-
-fdtd.addobject("bandstructure")
-fdtd.set("x span", ax)
-fdtd.set("y span", ay)
-fdtd.set("z span", az)
-fdtd.set("f1", f1)
-fdtd.set("f2", f2)
-fdtd.set("n monitors", 10)
-# -
-
-# zoom CAD view around simulation region
-fdtd.select("FDTD")
-fdtd.setview("extent")
-
-fdtd.save("photonic_crystal_bandstructure.fsp")
-fdtd.run()
-fdtd.runanalysis()
-
-# Visualize the spectrum from a single simulation
-single_spectrum = fdtd.getresult("bandstructure", "spectrum")
-fdtd.visualize(single_spectrum)
-
-print("Part 1 complete. Single simulation has been run. Next, we set up and run sweeps.")
 
 # Part 2: Set up and run sweeps to extract resonant frequencies and plot the bandstructure
 
@@ -147,104 +147,80 @@ norm_x = 2 * np.pi / ax
 norm_y = 2 * np.pi / ay
 norm_z = 2 * np.pi / az
 
-# Add Gamma-X sweep
-
-fdtd.addsweep()
-fdtd.setsweep("sweep", "name", "Gamma-X")
-fdtd.setsweep("Gamma-X", "type", "Ranges")
-fdtd.setsweep("Gamma-X", "number of points", 20)
-
-# set the sweep properties
-props_kx = {"Name": "kx", "Parameter": "::model::FDTD::kx", "Type": "Number", "Start": 0 * norm_x, "Stop": 0.5 * norm_x}
-fdtd.addsweepparameter("Gamma-X", props_kx)
-props_ky = {"Name": "ky", "Parameter": "::model::FDTD::ky", "Type": "Number", "Start": 0 * norm_y, "Stop": 0 * norm_y}
-fdtd.addsweepparameter("Gamma-X", props_ky)
-props_kz = {"Name": "kz", "Parameter": "::model::FDTD::kz", "Type": "Number", "Start": 0 * norm_z, "Stop": 0 * norm_z}
-fdtd.addsweepparameter("Gamma-X", props_kz)
-
-# define results
-result_fs = {"Name": "fs", "Result": "::model::bandstructure::fs"}
-fdtd.addsweepresult("Gamma-X", result_fs)
-result_spectrum = {"Name": "fs", "Result": "::model::bandstructure::spectrum"}
-fdtd.addsweepresult("Gamma-X", result_spectrum)
-
-# Add X-M sweep
-
-fdtd.addsweep()
-fdtd.setsweep("sweep", "name", "X-M")
-fdtd.setsweep("X-M", "type", "Ranges")
-fdtd.setsweep("X-M", "number of points", 20)
-
-# set the sweep properties
-props_kx = {"Name": "kx", "Parameter": "::model::FDTD::kx", "Type": "Number", "Start": 0.5 * norm_x, "Stop": 0.5 * norm_x}
-fdtd.addsweepparameter("X-M", props_kx)
-props_ky = {"Name": "ky", "Parameter": "::model::FDTD::ky", "Type": "Number", "Start": 0 * norm_y, "Stop": 0.5 * norm_y}
-fdtd.addsweepparameter("X-M", props_ky)
-props_kz = {"Name": "kz", "Parameter": "::model::FDTD::kz", "Type": "Number", "Start": 0 * norm_z, "Stop": 0 * norm_z}
-fdtd.addsweepparameter("X-M", props_kz)
-
-# define results
-result_fs = {"Name": "fs", "Result": "::model::bandstructure::fs"}
-fdtd.addsweepresult("X-M", result_fs)
-result_spectrum = {"Name": "fs", "Result": "::model::bandstructure::spectrum"}
-fdtd.addsweepresult("X-M", result_spectrum)
-
-# Add M-R sweep
-
-fdtd.addsweep()
-fdtd.setsweep("sweep", "name", "M-R")
-fdtd.setsweep("M-R", "type", "Ranges")
-fdtd.setsweep("M-R", "number of points", 20)
-
-# set the sweep properties
-props_kx = {"Name": "kx", "Parameter": "::model::FDTD::kx", "Type": "Number", "Start": 0.5 * norm_x, "Stop": 0.5 * norm_x}
-fdtd.addsweepparameter("M-R", props_kx)
-props_ky = {"Name": "ky", "Parameter": "::model::FDTD::ky", "Type": "Number", "Start": 0.5 * norm_y, "Stop": 0.5 * norm_y}
-fdtd.addsweepparameter("M-R", props_ky)
-props_kz = {"Name": "kz", "Parameter": "::model::FDTD::kz", "Type": "Number", "Start": 0 * norm_z, "Stop": 0.5 * norm_z}
-fdtd.addsweepparameter("M-R", props_kz)
-
-# define results
-result_fs = {"Name": "fs", "Result": "::model::bandstructure::fs"}
-fdtd.addsweepresult("M-R", result_fs)
-result_spectrum = {"Name": "fs", "Result": "::model::bandstructure::spectrum"}
-fdtd.addsweepresult("M-R", result_spectrum)
-
-# +
-# Run all the sweeps - this may take a few minutes
-print("Sweeps have been set up. Starting sweep run now.")
-fdtd.runsweep()
-print("Sweep run completed. Now retrieving and plotting results.")
-
-# If you have previously run the sweeps, you can load them using this line instead:
-# fdtd.loadsweep()
+# The total number of simulations will be num_points*3
+num_points = 5
 
 
-# +
-# Retrieve and analyze data from the sweeps
-fs_all = np.zeros((50, 60))  # 50 is the number of frequencies, and 60 bandstructure points = 20 points per sweep x 3 sweeps
+# Define a function to add the sweeps
+def add_bandstructure_sweep(sweep_name, x_start, x_stop, y_start, y_stop, z_start, z_stop):
+    """Set up a sweep for bandstructure calculations."""
+    fdtd.addsweep()
+    fdtd.setsweep("sweep", "name", sweep_name)
+    fdtd.setsweep(sweep_name, "type", "Ranges")
+    fdtd.setsweep(sweep_name, "number of points", num_points)
 
-sweepname = "Gamma-X"
-resonance = fdtd.getsweepresult(sweepname, "fs")  # Results are returned as Python dict
-resonance_fs = resonance["fs"]
-fs_all[0:50, 0:20] = resonance_fs
+    # set the sweep properties
+    props_kx = {"Name": "kx", "Parameter": "::model::FDTD::kx", "Type": "Number", "Start": x_start, "Stop": x_stop}
+    fdtd.addsweepparameter(sweep_name, props_kx)
+    props_ky = {"Name": "ky", "Parameter": "::model::FDTD::ky", "Type": "Number", "Start": y_start, "Stop": y_stop}
+    fdtd.addsweepparameter(sweep_name, props_ky)
+    props_kz = {"Name": "kz", "Parameter": "::model::FDTD::kz", "Type": "Number", "Start": z_start, "Stop": z_stop}
+    fdtd.addsweepparameter(sweep_name, props_kz)
 
-sweepname = "X-M"
-resonance = fdtd.getsweepresult(sweepname, "fs")  # Results are returned as Python dict
-resonance_fs = resonance["fs"]
-fs_all[0:50, 20:40] = resonance_fs
+    # define results
+    result_fs = {"Name": "fs", "Result": "::model::bandstructure::fs"}
+    fdtd.addsweepresult(sweep_name, result_fs)
+    result_spectrum = {"Name": "fs", "Result": "::model::bandstructure::spectrum"}
+    fdtd.addsweepresult(sweep_name, result_spectrum)
 
-sweepname = "M-R"
-resonance = fdtd.getsweepresult(sweepname, "fs")  # Results are returned as Python dict
-resonance_fs = resonance["fs"]
-fs_all[0:50, 40:60] = resonance_fs
 
+# Now add the sweeps to the file
+with lumapi.FDTD(filename, hide=False) as fdtd:
+    # Add Gamma-X sweep
+    add_bandstructure_sweep("Gamma-X", 0 * norm_x, 0.5 * norm_x, 0 * norm_y, 0 * norm_y, 0 * norm_z, 0 * norm_z)
+    # Add X-M sweep
+    add_bandstructure_sweep("X-M", 0.5 * norm_x, 0.5 * norm_x, 0 * norm_y, 0.5 * norm_y, 0 * norm_z, 0 * norm_z)
+    # Add M-R sweep
+    add_bandstructure_sweep("M-R", 0.5 * norm_x, 0.5 * norm_x, 0.5 * norm_y, 0.5 * norm_y, 0 * norm_z, 0.5 * norm_z)
+    fdtd.save(filename)
+    print("Sweeps have been set up.")
 # -
 
-k = np.linspace(1, 60, 60)
+# Now run all the sweeps - this may take a few minutes
+with lumapi.FDTD(filename, hide=False) as fdtd:
+    print("Running sweeps...")
+    # fdtd.runsweep()
+    # fdtd.save(filename)
+    # If you have previously run the sweeps, you can load them using this line instead:
+    fdtd.loadsweep()
+print("Sweeps have been run.")
+
+# Retrieve and analyze data from the sweeps
+with lumapi.FDTD(filename, hide=False) as fdtd:
+    fs_all = np.zeros((50, 3 * num_points))  # 50 is the number of frequencies, and there are 3*num_points bandstructure points
+
+    sweepname = "Gamma-X"
+    resonance = fdtd.getsweepresult(sweepname, "fs")  # Results are returned as Python dict
+    resonance_fs = resonance["fs"]
+    fs_all[0:50, 0:num_points] = resonance_fs
+
+    sweepname = "X-M"
+    resonance = fdtd.getsweepresult(sweepname, "fs")  # Results are returned as Python dict
+    resonance_fs = resonance["fs"]
+    fs_all[0:50, num_points : 2 * num_points] = resonance_fs
+
+    sweepname = "M-R"
+    resonance = fdtd.getsweepresult(sweepname, "fs")  # Results are returned as Python dict
+    resonance_fs = resonance["fs"]
+    fs_all[0:50, 2 * num_points : 3 * num_points] = resonance_fs
+
+
+k = np.linspace(1, 3 * num_points, 3 * num_points)
 c = 3 * 10**8
+plt.figure()
 for f in range(0, 50):
     plt.scatter(k, fs_all[f] * ax / c)
 plt.xlabel("k (Gamma-X-M-R-Gamma")
 plt.ylabel("Resonant Frequency f (Hz*a/c)")
-plt.show()
+plt.show(block=False)
+plt.pause(10)
